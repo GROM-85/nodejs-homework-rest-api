f0220cf649559f813d6579567e129858dcc3390f
GROM-85 <richfron69@gmail.com>
2023-05-01 19:59:40 +0200

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 require('dotenv').config();
 
 const contactsRouter = require('./routes/api/contacts');
+const authRouter = require('./routes/api/auth');
 const app = express() // to create our app express server
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -12,6 +13,7 @@ app.use(logger(formatsLogger)) // creatiion of middlewares, order is matter
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
---
+++ models/user.js
@@ -0,0 +1,68 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const { handleSaveErrors } = require("../helpers");
+
+const emailRegExp = /^[A-Za-z0-9_!#$%&'*+=?`{|}~^.-]+@[A-Za-z0-9.-]+$/;
+const allowedSubscriptions = ["starter", "pro", "business"];
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: emailRegExp,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: true,
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+    subscription: {
+      type: String,
+      default: "starter",
+      enum: allowedSubscriptions,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleSaveErrors);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegExp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegExp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const subscriptionSchema = Joi.object({
+  subscription: Joi.string()
+    .valid(...allowedSubscriptions)
+    .required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+  subscriptionSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
