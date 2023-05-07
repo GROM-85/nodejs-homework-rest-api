b40468f85559f724068e065dc4f5457fa68b18d7
GROM-85 <richfron69@gmail.com>
2023-05-07 19:14:34 +0200

    hw06

--- helpers/index.js
+++ helpers/index.js
@@ -1,11 +1,13 @@
 const RequestError = require('./requesterror');
 const ctrlWrapper = require('./ctrlWrapper');
 const handleSaveErrors = require('./handleSaveErrors');
-const resizeAndUpload = require('./resize')
+const resizeAndUpload = require('./resize');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
     ctrlWrapper,
     RequestError,
     handleSaveErrors,
-    resizeAndUpload
+    resizeAndUpload,
+    sendEmail,
 }
